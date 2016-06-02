028206361f6cc1b5eb2a28021feb44efb3d8f28c
aprokopenko <aprokopenko@brightgroove.com>
2016-06-02 15:59:13 +0300

    removed RxFunctional from MainFragment. Moved to TripProcessor++

--- app/src/main/java/com/example/aprokopenko/triphelper/listener/LocationListener.java
+++
@@ -1,7 +0,0 @@
-//package com.example.aprokopenko.triphelper.listener;
-//
-//import android.location.Location;
-//
-//public interface LocationListener {
-//    void locationChanged(Location location);
-//}
