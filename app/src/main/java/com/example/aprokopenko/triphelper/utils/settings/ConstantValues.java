922cb707bea0a9f1bd788edbab2ba5d871071502
aprokopenko <aprokopenko@brightgroove.com>
2016-06-22 18:55:13 +0300

    code cleanup

--- app/src/main/java/com/example/aprokopenko/triphelper/utils/settings/ConstantValues.java
+++ app/src/main/java/com/example/aprokopenko/triphelper/utils/settings/ConstantValues.java
@@ -27,6 +27,7 @@ public class ConstantValues {
     public static final boolean DEBUG_MODE         = false; //todo change for release to false
     public static final boolean LOGGING_ENABLED    = false; //todo change for release to false
 
+
     public static final int TEXT_ANIM_DURATION             = 900;
     public static final int SPEEDOMETER_TEXT_ANIM_DURATION = 900;
 
